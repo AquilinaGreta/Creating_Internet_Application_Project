a8893570aa9e60b4def092a61832a0840dbbf276
AquilinaGreta <90396995+AquilinaGreta@users.noreply.github.com>
2022-03-20 14:23:06 +0100

    Create userSelectionSignup.php

---
+++ userSelectionSignup.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Post creation</title>
+</head>
+
+<body>
+   
+        
+        <h1>Sign up</h1>
+        <div>
+           <h2>Are you a Creator or a Company?</h2>
+        </div>
+
+        <div>
+        <form method="POST" action = "./signUpContentCreator.php">
+            <button type="submit">content Creator</button>
+            </form>
+
+            <form method="POST" action = "./signUpCompany.php">
+            <button type="sumbit"  action = "./signUpCompany.php">company</button>
+            </form>
+            
+        </div>
+    
+</body>
+
+</html>
