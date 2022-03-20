4d7401cb4fa1811df5993f10a5b69506c492e345
AquilinaGreta <90396995+AquilinaGreta@users.noreply.github.com>
2022-03-20 14:22:46 +0100

    Create check_VAT_Comp.php

---
+++ check_VAT_Comp.php
@@ -0,0 +1,36 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+
+include("./db_files/connection.php");
+
+//Check if mail is already in DB
+
+$vat = $_POST['vat'];
+
+// Using prepared statements almost eliminates the possibility of SQL Injection.
+/*$preparedQuery = $mysqliConnection->prepare("SELECT * FROM $db_tab_company WHERE `email` = (?)");
+$preparedQuery->bind_param("s", $email);
+$preparedQuery->execute();
+
+$result = $preparedQuery->get_result(); */
+
+$result = $mysqliConnection->query("SELECT * FROM $db_tab_company WHERE `VAT_number` = '$vat'");
+
+/* Get the number of rows in the result set */
+$row_cnt = $result->num_rows;
+
+if($row_cnt > 0){
+    echo ('match');
+}
+else if($row_cnt == 0){
+    echo ('no match');
+}
+else{
+    echo ('error');
+}
+
+
+?>
